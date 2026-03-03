b4f46734828499324514f760ea184fb65a1b20b2
Udo Nonner <udo.nonner@franzk.net>
2026-03-03 14:35:19 +0100

    fix / improve

--- files/scripts/pdf/merge-pdf.sh
+++ files/scripts/pdf/merge-pdf.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
 DEBUG=0
-
 # Create an array from the string
 readarray -t SELECTED_PATHS <<< "${NAUTILUS_SCRIPT_SELECTED_FILE_PATHS}"
+# get directory to open using the last element in the array of selected files
+OPEN_DIR=$(dirname ${SELECTED_PATHS[1]})
 
 # Check if there are at least two files selected
 if [ "${#}" -lt 2 ]; then
@@ -12,17 +13,27 @@ if [ "${#}" -lt 2 ]; then
 fi
 
 if ! which pdfunite; then
-	${ZENITY} --error --title="Error" --text="This script requires the poppler-utils package, which is not installed. Please install it and try again."
+	zenity --error --title="Error" --text="This script requires the poppler-utils package, which is not installed. Please install it and try again."
 	exit 1
 fi
 
-# get directory to open using the last element in the array of selected files
-OPEN_DIR=$(dirname ${SELECTED_PATHS[1]})
-
 if ((DEBUG)); then
-  echo "SELECTED_PATHS[1]: ${SELECTED_PATHS[1]}"
+  echo "SELECTED_PATHS[1]: ${SELECTED_PATHS[@]}"
   echo "OPEN_DIR: ${OPEN_DIR}"
 fi
+
+# Loop over selected files
+for selected_path in ${SELECTED_PATHS[@]}; do
+		# Check if it's a PDF file
+	# ignoring case for 'pdf'; as far as I know, the slash before (sth/pdf) is universal mimetype output. In most cases we can even expect 'application/pdf' (portability issues?)
+	file --brief --mime-type "${selected_path}" | grep -iq "/pdf"
+	file_exit_code=${?}
+	if [[ ${file_exit_code} -ne 0 ]]; then
+		echo "Not a pdf: ${selected_path}"
+		zenity --error --title="Error" --text="Dies ist kein PDF:\n<b>${selected_path}</b>\n\nBitte nur PDFs auswählen!"
+		exit 1
+	fi
+done
 
 # Ask the user for the name of the output file
 # OUT_NAME=$(zenity --entry --title="Filename" --text="Name of the merged file:")
@@ -48,7 +59,9 @@ fi
 CMD_OUTPUT=$(pdfunite "${@}" "${OUT_PATH}")
 CMD_EXIT_CODE=${?}
 
-echo ${CMD_OUTPUT}
+if ((DEBUG)); then
+	echo ${CMD_OUTPUT}
+fi
 
 if [ ${CMD_EXIT_CODE} -eq 0 ]; then
     zenity --info --text="PDFs successfully merged into:\n<b>${OUT_PATH}</b>"
